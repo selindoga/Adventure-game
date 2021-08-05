43b51676d170691bd4cd9ddc1f18b713fd7aeacc
Selin Doga Orhan <73192627+selindoga@users.noreply.github.com>
2021-08-05 20:13:11 +0300

    made horizontal player movement from joystick

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,18 +1,45 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    private float Sensitivity = 0.35f;
+
+    public Joystick joystick;
+    private Rigidbody2D rb;
+    private Vector2 HorizontalMove;
+
+    private float jHorizontal;
+    private float jVertical;
+
+
+    private void Awake()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
     }
 
-    // Update is called once per frame
-    void Update()
+
+    void Start()
     {
+        HorizontalMove = new Vector2(1f, 0f);
+    }
+    void FixedUpdate()
+    {
+        jHorizontal = joystick.Horizontal;
+        jVertical = joystick.Vertical;
         
+        // Rigidbody2D.velocity is good with dynamic
+        // Rigidbody2D.MovePosition is good with kinematic
+        
+        if (jHorizontal >= Sensitivity)
+        {
+            rb.velocity = HorizontalMove;
+        } 
+        else if (jHorizontal <= -Sensitivity)
+        {
+            rb.velocity = -HorizontalMove;
+        }
     }
 }
