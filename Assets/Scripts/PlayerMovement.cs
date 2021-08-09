9ff49f1d1c0b2479aed529b96913c4df7dfcd919
Selin Doga Orhan <73192627+selindoga@users.noreply.github.com>
2021-08-09 21:17:59 +0300

    jump function and its button implemented
    with a beautiful bug of course (cannot be used with multi touches)

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,14 +1,13 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.IO.IsolatedStorage;
 using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    
-    public LayerMask groundMask;
     public Joystick joystick;
-    
+    public LayerMask groundMask;
     public Transform groundCheck;
     public Transform groundCheck1;
     public Transform groundCheck2;
@@ -16,31 +15,34 @@ public class PlayerMovement : MonoBehaviour
 
     private Rigidbody2D rb;
     private Vector2 HorizontalMove;
-    private float jHorizontal;
-    private float jVertical;
-    private float Sensitivity = 0.35f;
-    private float movingSpeed = 150f;
+    private Vector2 VerticalMove;
     private Vector2 lowestGroundCheck;
-    private Vector3[] groundCheckArray = new Vector3[4];
+    private float jHorizontal;
+    //private float jVertical;
+    private float Sensitivity = 0.35f;
+    private float movingSpeed = 70f;
+    private float jumpingSpeed = 10f;
     private float groundDistance = 0.1f;
+    private Vector3[] groundCheckArray = new Vector3[4];
     
     [SerializeField]
     private bool isGrounded;
 
+    public TouchPhase touch;
     private void Awake()
     {
         rb = GetComponent<Rigidbody2D>();
     }
 
-
     void Start()
     {
         HorizontalMove = new Vector2(1f, 0f);
+        VerticalMove = new Vector2(0f, 1f);
     }
     void FixedUpdate()
     {
         jHorizontal = joystick.Horizontal;
-        jVertical = joystick.Vertical;
+        // jVertical = joystick.Vertical;
         
         // Rigidbody2D.velocity is good with dynamic
         // Rigidbody2D.MovePosition is good with kinematic
@@ -76,5 +78,13 @@ public class PlayerMovement : MonoBehaviour
                 _vector = vector;
         }
         return _vector;
+    }
+
+    public void Jump()
+    {
+        if (isGrounded)
+        {
+            rb.velocity = VerticalMove * jumpingSpeed;
+        }
     }
 }
